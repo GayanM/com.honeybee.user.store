e05718e31a06dfa1e489693c84c2c344c76e27c8
Gayan <gmgunawardana@gmail.com>
2017-11-09 17:21:52 +0000

    Adding minor code refactoring

--- src/main/java/com/honeybee/user/store/internal/HoneybeeJDBCUserStoreManagerServiceComponent.java
+++ src/main/java/com/honeybee/user/store/internal/HoneybeeJDBCUserStoreManagerServiceComponent.java
@@ -9,9 +9,6 @@ import org.wso2.carbon.user.core.service.RealmService;
 
 /**
  * @scr.component name="com.climate.user.store.internal.component" immediate="true"
- * @scr.reference name="realm.service"
- * interface="org.wso2.carbon.user.core.service.RealmService"cardinality="1..1"
- * policy="dynamic" bind="setRealmService" unbind="unsetRealmService"
  */
 public class HoneybeeJDBCUserStoreManagerServiceComponent {
     private static Log log = LogFactory.getLog(HoneybeeJDBCUserStoreManagerServiceComponent.class);
@@ -23,28 +20,14 @@ public class HoneybeeJDBCUserStoreManagerServiceComponent {
         try {
             UserStoreManager climateUserStoreManager = new HoneybeeJDBCUserStoreManager();
             ctxt.getBundleContext().registerService(UserStoreManager.class.getName(), climateUserStoreManager, null);
-            log.info("ClimateUserStoreManager bundle activated successfully..");
+            log.info("HoneybeeUserStoreManager bundle activated successfully..");
         } catch (Exception e) {
-            log.error("Failed to activate climateUserStoreManager ", e);
+            log.error("Failed to activate HoneybeeUserStoreManager ", e);
         }
     }
 
     protected void deactivate(ComponentContext ctxt) {
-        log.info("CustomUserStoreManager bundle is deactivated");
+        log.info("HoneybeeUserStoreManager bundle is deactivated");
 
-    }
-
-    protected void setRealmService(RealmService realmService) {
-        log.debug("Setting the Realm Service");
-        HoneybeeJDBCUserStoreManagerServiceComponent.realmService = realmService;
-    }
-
-    protected void unsetRealmService(RealmService realmService) {
-        log.debug("UnSetting the Realm Service");
-        HoneybeeJDBCUserStoreManagerServiceComponent.realmService = null;
-    }
-
-    public static RealmService getRealmService() {
-        return realmService;
     }
 }
